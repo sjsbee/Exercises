ec430beda2a90578f06e07d2ddb949723ea2eb1b
sjsbee <93555209+sjsbee@users.noreply.github.com>
2022-06-06 04:53:36 -0500

    Add files via upload

---
+++ compareAge.js
@@ -0,0 +1,18 @@
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	compareAge(other) {
+		if (other.age > this.age){
+		return `${other.name} is older than me.`;
+		}
+		else if (other.age === this.age){
+			return `${other.name} is the same age as me.`;
+		} else {
+			return `${other.name} is younger than me.`;
+}
+}
+		
+	}
